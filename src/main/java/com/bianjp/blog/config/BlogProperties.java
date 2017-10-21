adc60126501f096fd89ea2d41d701522463c97c4
Bian Jiaping <ssbianjp@gmail.com>
2017-10-21 11:22:34 +0800

    Disable disqus in development environment

--- src/main/java/com/bianjp/blog/config/BlogProperties.java
+++ src/main/java/com/bianjp/blog/config/BlogProperties.java
@@ -1,12 +1,40 @@
 package com.bianjp.blog.config;
 
+import org.hibernate.validator.constraints.NotEmpty;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 
 @Component
 @ConfigurationProperties(prefix = "blog")
+@Validated
 public class BlogProperties {
-  private String title;
+  /** Root url of the blog, without ending slash. eg. https://bianjp.com */
+  @NotEmpty
+  @Pattern.List({
+    @Pattern(regexp = "https?://[\\w.-_]+", message = "Invalid URL"),
+    @Pattern(regexp = ".*[^/]$", message = "Should not end with /")
+  })
+  private String host;
+
+  /** Title of the blog. Used for HTML <title> */
+  @NotEmpty private String title;
+
+  /** Disqus configuration */
+  @Valid
+  private final Disqus disqus = new Disqus();
+
+  public String getHost() {
+    return host;
+  }
+
+  public void setHost(String host) {
+    this.host = host;
+  }
 
   public String getTitle() {
     return title;
@@ -14,5 +42,34 @@ public class BlogProperties {
 
   public void setTitle(String title) {
     this.title = title;
+  }
+
+  public Disqus getDisqus() {
+    return disqus;
+  }
+
+  /** Disqus configuration */
+  public static class Disqus {
+    /** Enable comment */
+    @NotNull private Boolean enabled;
+
+    /** Shortname used to uniquely identify your website on Disqus */
+    @NotEmpty private String shortname;
+
+    public Boolean getEnabled() {
+      return enabled;
+    }
+
+    public void setEnabled(Boolean enabled) {
+      this.enabled = enabled;
+    }
+
+    public String getShortname() {
+      return shortname;
+    }
+
+    public void setShortname(String shortname) {
+      this.shortname = shortname;
+    }
   }
 }
--- src/main/resources/templates/frontend/included/disqus.ftl
+++ src/main/resources/templates/frontend/included/disqus.ftl
@@ -1,16 +1,19 @@
+<#if blog.disqus.enabled>
 <div id="disqus_thread"></div>
 <script>
   var disqus_config = function () {
-    this.page.url = 'https://bianjp.com/posts/${post.id}';
+    this.page.url = '${blog.host}/posts/${post.id}';
     this.page.identifier = 'post-${post.id}';
     this.page.title = '${post.title?js_string}';
   };
 
-  (function() { // DON'T EDIT BELOW THIS LINE
+  (function () { // DON'T EDIT BELOW THIS LINE
     var d = document, s = d.createElement('script');
-    s.src = 'https://bianjp-blog.disqus.com/embed.js';
+    s.src = 'https://${blog.disqus.shortname}.disqus.com/embed.js';
     s.setAttribute('data-timestamp', +new Date());
     (d.head || d.body).appendChild(s);
   })();
 </script>
-<noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+<noscript>Please enable JavaScript to view the
+  <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+</#if>
